306ac224a0bec84b590ff83ee291f45431242b08
linuxloser137 <linuxloser137@gmail.com>
2023-07-09 01:26:50 -0400

    cleanup

--- IT213_Young_Unit1.java
+++ IT213_Young_Unit1.java
@@ -38,9 +38,7 @@ public class IT213_Young_Unit1
 		System.out.println("City: " + city);
 		System.out.println("State: " + state);
 		System.out.println("Zip Code: " + zip);
-		
 		System.out.println("The number of units taken is: " + intunitsTaken);
-		
 		//format tuition values and monthly payments as currency and display
        	System.out.println("The tuition before discount is " + "$" + tuition);
 		System.out.println("The tuition after discount is " + "$" + afterDiscount);
